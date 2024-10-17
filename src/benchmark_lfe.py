a5adbe471d006e06448a544acc1dcbaa9fc83e19
Andrew Lapp <andrew@github.rew.la>
2024-10-17 03:31:08 -0400

    refactor benchmarks, split into warmup/runtime/total, tokenize samples before running

--- src/benchmark_lfe.py
+++ src/benchmark_lfe.py
@@ -8,61 +8,100 @@ from transformers import AutoTokenizer
 from .data import json_cases, models, regex_cases
 
 
-class LMFormatEnforcerRegex:
+class LMFormatEnforcerBenchmark:
+    def do_setup(self, model, samples):
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            model, clean_up_tokenization_spaces=True
+        )
+        self.tokenizer_data = build_token_enforcer_tokenizer_data(self.tokenizer)
+        self.all_tokenized_samples = [
+            self.tokenizer.encode(sample) for sample in samples
+        ]
+
+    def _exhaust_samples(self, token_enforcer):
+        for sample_tokens in self.all_tokenized_samples:
+            for i in range(len(sample_tokens)):
+                _ = token_enforcer.get_allowed_tokens(sample_tokens[: i + 1])
+
+    def _get_first_token(self, token_enforcer):
+        """Get first token to verify lazy index is fully warmed up"""
+        _ = token_enforcer.get_allowed_tokens(self.all_tokenized_samples[0][:1])
+
+    def teardown(self, *args):
+        del self.tokenizer_data
+
+
+class LMFormatEnforcerRegex(LMFormatEnforcerBenchmark):
     params = [models, regex_cases.keys()]
     param_names = ["model", "regex_name"]
     timeout = 1200
 
-    def setup(self, model, _):
-        """Set up the benchmark.
+    def setup(self, model, regex_name):
+        samples = regex_cases[regex_name]["samples"]
+        self.do_setup(model, samples)
 
-        We convert the tokenizer during set up as this only
-        needs to be done once for a given model.
+    def _get_enforcer(self, regex_name):
+        pattern = regex_cases[regex_name]["regex"]
+        parser = RegexParser(pattern)
+        return TokenEnforcer(self.tokenizer_data, parser)
 
-        """
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            model, clean_up_tokenization_spaces=True
-        )
+    def time_lfe_total(self, _, regex_name):
+        enforcer = self._get_enforcer(regex_name)
+        self._exhaust_samples(enforcer)
 
-    def time_lfe(self, _, regex_name):
-        regex_string = regex_cases[regex_name]["regex"]
-        regex_samples = regex_cases[regex_name]["samples"]
-
-        parser = RegexParser(regex_string)
-        tokenizer_data = build_token_enforcer_tokenizer_data(self.tokenizer)
-        token_enforcer = TokenEnforcer(tokenizer_data, parser)
-
-        for regex_sample in regex_samples:
-            regex_sample_tokens = self.tokenizer.encode(regex_sample)
-            for i in range(len(regex_sample_tokens)):
-                _ = token_enforcer.get_allowed_tokens(regex_sample_tokens[: i + 1])
+    def time_lfe_first_token(self, _, regex_name):
+        enforcer = self._get_enforcer(regex_name)
+        self._get_first_token(enforcer)
 
 
-class LMFormatEnforcerJsonSchema:
+class LMFormatEnforcerRegexRunTime(LMFormatEnforcerRegex):
+    """Class which warms-up enforcer in setup steps"""
+
+    def setup(self, model, regex_name):
+        samples = regex_cases[regex_name]["samples"]
+        self.do_setup(model, samples)
+
+        # ensure warmed up so we're only measuring runtime
+        self.enforcer = self._get_enforcer(regex_name)
+        self._get_first_token(self.enforcer)
+
+    def time_lfe_runtime(self, *args):
+        self._exhaust_samples(self.enforcer)
+
+
+class LMFormatEnforcerJsonSchema(LMFormatEnforcerBenchmark):
     params = [models, json_cases.keys()]
     param_names = ["model", "json_schema_name"]
-    timeout = 1200
+    timeout = 600
 
-    def setup(self, model, _):
-        """Set up the benchmark.
+    def setup(self, model, json_schema_name):
+        samples = json_cases[json_schema_name]["samples"]
+        self.do_setup(model, samples)
 
-        We convert the tokenizer during set up as this only
-        needs to be done once for a given model.
+    def _get_enforcer(self, json_schema_name):
+        schema = json_cases[json_schema_name]["schema"]
+        parser = JsonSchemaParser(schema)
+        return TokenEnforcer(self.tokenizer_data, parser)
 
-        """
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            model, clean_up_tokenization_spaces=True
-        )
+    def time_lfe_total(self, _, json_schema_name):
+        enforcer = self._get_enforcer(json_schema_name)
+        self._exhaust_samples(enforcer)
 
-    def time_lfe(self, _, json_schema_name):
-        json_string = json_cases[json_schema_name]["schema"]
-        json_samples = json_cases[json_schema_name]["samples"]
+    def time_lfe_first_token(self, _, json_schema_name):
+        enforcer = self._get_enforcer(json_schema_name)
+        self._get_first_token(enforcer)
 
-        parser = JsonSchemaParser(json_string)
-        tokenizer_data = build_token_enforcer_tokenizer_data(self.tokenizer)
-        token_enforcer = TokenEnforcer(tokenizer_data, parser)
 
-        for json_sample in json_samples:
-            json_sample_tokens = self.tokenizer.encode(json_sample)
-            for i in range(len(json_sample_tokens)):
-                _ = token_enforcer.get_allowed_tokens(json_sample_tokens[: i + 1])
+class LMFormatEnforcerJsonSchemaRunTime(LMFormatEnforcerJsonSchema):
+    """Class which warms-up enforcer in setup steps"""
+
+    def setup(self, model, json_schema_name):
+        samples = json_cases[json_schema_name]["samples"]
+        self.do_setup(model, samples)
+
+        # ensure warmed up so we're only measuring runtime
+        self.enforcer = self._get_enforcer(json_schema_name)
+        self._get_first_token(self.enforcer)
+
+    def time_lfe_runtime(self, *args):
+        self._exhaust_samples(self.enforcer)
